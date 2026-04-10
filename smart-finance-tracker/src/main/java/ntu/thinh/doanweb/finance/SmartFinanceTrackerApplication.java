41ce573c49d0aae3bafe4bd662959b8e803ef79d
Boydayroi <hanguyentienthinh@gmail.com>
2026-04-10 14:48:03 +0700

    .

---
+++ smart-finance-tracker/src/main/java/ntu/thinh/doanweb/finance/SmartFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package ntu.thinh.doanweb.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinanceTrackerApplication.class, args);
+	}
+
+}
---
+++ smart-finance-tracker/src/test/java/ntu/thinh/doanweb/finance/SmartFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.thinh.doanweb.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
